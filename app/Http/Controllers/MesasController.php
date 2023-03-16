da9ee7743fdaf85303087d4ebdfbaa4cb26e7d25
Mao9207 <omarmao.101@hotmail.com>
2023-03-16 15:35:40 -0500

    Creacion de productos

---
+++ app/Http/Controllers/MesasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mesas;
+use Illuminate\Http\Request;
+
+class MesasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mesas  $mesas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mesas $mesas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mesas  $mesas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mesas $mesas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mesas  $mesas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mesas $mesas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mesas  $mesas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mesas $mesas)
+    {
+        //
+    }
+}
